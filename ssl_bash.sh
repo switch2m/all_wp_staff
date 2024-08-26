13000d39e62571935e7a7ab07cfa106c976e01cb
switchit <72983573+switch2m@users.noreply.github.com>
2024-08-26 13:41:40 +0100

    Update ssl_bash.sh

--- ssl_bash.sh
+++ ssl_bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt install certbot python3-certbot-apache
-sudo certbot --apache -d testwebsite.com -d www.testwebsite.com
+sudo certbot --apache -d unleashedrecipes.com -d www.unleashedrecipes.com
 systemctl stop apache2
-certbot certonly --standalone --preferred-challenges http -d testwebsite.com,www.testwebsite.com
+certbot certonly --standalone --preferred-challenges http -d unleashedrecipes.com,www.unleashedrecipes.com
 systemctl start apache2
 systemctl status apache2
