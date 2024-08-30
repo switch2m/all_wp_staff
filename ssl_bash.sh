c6377264fe3810fc238513c675f0343de3d70367
switchit <72983573+switch2m@users.noreply.github.com>
2024-08-30 19:49:11 +0100

    Update ssl_bash.sh

--- ssl_bash.sh
+++ ssl_bash.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt install certbot python3-certbot-apache
-sudo certbot --apache -d mayssam.shop -d www.mayssam.shop
+sudo certbot --apache -d flavorfullrecipes.com -d www.flavorfullrecipes.com
 sudo systemctl stop apache2
-certbot certonly --standalone --preferred-challenges http -d mayssam.shop,www.mayssam.shop
+certbot certonly --standalone --preferred-challenges http -d flavorfullrecipes.com,www.flavorfullrecipes.com
 sudo systemctl start apache2
 sudo systemctl status apache2
   
